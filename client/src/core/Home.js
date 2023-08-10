371a81f82188f92a7272e110d4d4ac4294bfcddd
Liharsha <liharsha2022@gmail.com>
2023-08-09 20:27:46 -0400

    admin auth and core frontend files

---
+++ client/src/core/Home.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import Layout from './Layout';
+import { getProducts } from './apiCore';
+import Card from './Card';
+import Search from './Search';
+import 'fontsource-roboto';
+import Copyright from './Copyright';
+
+const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState([]);
+
+  const loadProductsBySell = () => {
+    getProducts('sold').then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts('createdAt').then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
+  return (
+    <Layout
+      title='🎉 Exciting News! 🎉 Get ready for massive savings with our exclusive 20% OFF promotion! '
+      description="🛍️Whether you're looking for fashion, electronics, home decor, or more, now is the perfect time to shop and enjoy incredible discounts. Hurry and take advantage of the 20% discount before it's gone! Shop now and save big! 💸🛒 #ShopSmart #SavingsGalore"
+      className='container-fluid'
+    >
+      <Search />
+      <div className='row'>
+        <div className='col-md-1'></div>
+        <div className='col-md-10'>
+          <h2 className='mb-2'>New Arrivals</h2>
+          <div className='row'>
+            {productsByArrival.map((product, i) => (
+              <div key={i} className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
+                <Card product={product} />
+              </div>
+            ))}
+          </div>
+
+          <h2 className='mb-2 mt-4'>Best Sellers</h2>
+          <div className='row'>
+            {productsBySell.map((product, i) => (
+              <div key={i} className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
+                <Card product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='col-md-1'></div>
+      </div>
+
+      <Copyright />
+    </Layout>
+  );
+};
+
+export default Home;
